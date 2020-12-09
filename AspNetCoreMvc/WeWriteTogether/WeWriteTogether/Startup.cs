7aef42b92d0a9c64bcb93c41f6fff229b8630906
Enache-Ionut <enachestoianionut@gmail.com>
2020-12-09 14:22:43 +0200

    configure password requirements for user account

--- AspNetCoreMvc/WeWriteTogether/WeWriteTogether/Startup.cs
+++ AspNetCoreMvc/WeWriteTogether/WeWriteTogether/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using WeWriteTogether.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WeWriteTogether.Data;
 
 namespace WeWriteTogether
 {
@@ -32,6 +26,16 @@ namespace WeWriteTogether
               Configuration.GetConnectionString("DefaultConnection")));
       services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
           .AddEntityFrameworkStores<ApplicationDbContext>();
+
+      services.Configure<IdentityOptions>(options =>
+      {
+        // Password settings
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireDigit = true;
+        options.Password.RequiredLength = 8;
+        options.Password.RequireUppercase = false;
+      });
+
       services.AddControllersWithViews();
       services.AddRazorPages();
     }
