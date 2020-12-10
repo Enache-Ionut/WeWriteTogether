ee4053f567c42a9341edd51817bd021e30383776
Enache-Ionut <enachestoianionut@gmail.com>
2020-12-10 12:56:16 +0200

    include the email service into the services list using dependency injection

--- AspNetCoreMvc/WeWriteTogether/WeWriteTogether/Areas/Identity/IdentityHostingStartup.cs
+++ AspNetCoreMvc/WeWriteTogether/WeWriteTogether/Areas/Identity/IdentityHostingStartup.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.DependencyInjection;
-using WeWriteTogether.Data;
 
 [assembly: HostingStartup(typeof(WeWriteTogether.Areas.Identity.IdentityHostingStartup))]
 namespace WeWriteTogether.Areas.Identity
 {
-    public class IdentityHostingStartup : IHostingStartup
+  public class IdentityHostingStartup : IHostingStartup
+  {
+    public void Configure(IWebHostBuilder builder)
     {
-        public void Configure(IWebHostBuilder builder)
-        {
-            builder.ConfigureServices((context, services) => {
-            });
-        }
+      builder.ConfigureServices((context, services) =>
+      {
+        services.AddTransient<IEmailSender, EmailSender>();
+      });
     }
+  }
 }
