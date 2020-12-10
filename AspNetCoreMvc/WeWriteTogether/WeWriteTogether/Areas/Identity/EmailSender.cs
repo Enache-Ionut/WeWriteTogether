4f24f4584689fdc9841a7bebbf2f6ae53afc4ec3
Enache-Ionut <enachestoianionut@gmail.com>
2020-12-10 12:55:14 +0200

    dedicated class for sending emails using the Sendgrid api

---
+++ AspNetCoreMvc/WeWriteTogether/WeWriteTogether/Areas/Identity/EmailSender.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace WeWriteTogether.Areas.Identity
+{
+  public class EmailSender : IEmailSender
+  {
+    private readonly string apiKey;
+    private readonly string fromName;
+    private readonly string fromEmail;
+
+    public EmailSender(IConfiguration config)
+    {
+      apiKey = config["SendGrid:ApiKey"];
+      fromName = config["SendGrid:FromName"];
+      fromEmail = config["SendGrid:FromEmail"];
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+      var client = new SendGridClient(apiKey);
+      var msg = new SendGridMessage()
+      {
+        From = new EmailAddress(fromEmail, fromName),
+        Subject = subject,
+        PlainTextContent = htmlMessage,
+        HtmlContent = htmlMessage
+      };
+      msg.AddTo(new EmailAddress(email));
+
+      // Disable click tracking
+      msg.SetClickTracking(false, false);
+
+      await client.SendEmailAsync(msg);
+
+    }
+  }
+}
