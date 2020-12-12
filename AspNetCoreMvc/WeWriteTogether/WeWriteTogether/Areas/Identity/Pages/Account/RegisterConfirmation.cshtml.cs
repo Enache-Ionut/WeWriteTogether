c4bba1ef77f892c6c39d48080027893aa7f35ab2
Enache-Ionut <enachestoianionut@gmail.com>
2020-12-12 19:10:03 +0200

    replace the IdentityUser class with the new ApplicationUser class

--- AspNetCoreMvc/WeWriteTogether/WeWriteTogether/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ AspNetCoreMvc/WeWriteTogether/WeWriteTogether/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -4,16 +4,17 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
+using WeWriteTogether.Data;
 
 namespace WeWriteTogether.Areas.Identity.Pages.Account
 {
   [AllowAnonymous]
   public class RegisterConfirmationModel : PageModel
   {
-    private readonly UserManager<IdentityUser> _userManager;
+    private readonly UserManager<ApplicationUser> _userManager;
     private readonly IEmailSender _sender;
 
-    public RegisterConfirmationModel(UserManager<IdentityUser> userManager, IEmailSender sender)
+    public RegisterConfirmationModel(UserManager<ApplicationUser> userManager, IEmailSender sender)
     {
       _userManager = userManager;
       _sender = sender;
