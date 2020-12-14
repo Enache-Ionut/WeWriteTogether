7aa66d9992a5a1a560937206a50821b5bd748c6e
Enache-Ionut <enachestoianionut@gmail.com>
2020-12-14 10:50:08 +0200

    add the user FullName claim to the the claims collection

---
+++ AspNetCoreMvc/WeWriteTogether/WeWriteTogether/Areas/Identity/ApplicationUserClaimsPrincipalFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WeWriteTogether.Data;
+
+namespace WeWriteTogether.Areas.Identity
+{
+  public class ApplicationUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser>
+  {
+    public ApplicationUserClaimsPrincipalFactory(
+        UserManager<ApplicationUser> userManager,
+        IOptions<IdentityOptions> options
+      ) : base(userManager, options)
+    {
+    }
+
+    protected override async Task<ClaimsIdentity> GenerateClaimsAsync(ApplicationUser user)
+    {
+      var identity = await base.GenerateClaimsAsync(user);
+
+      identity.AddClaim(new Claim("FullName", user.FullName));
+      return identity;
+    }
+
+
+  }
+}
